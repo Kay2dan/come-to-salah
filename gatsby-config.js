bfdd6e22488e21c0b5c9d40eae65be8fc90e5573
kayote <kay2dan@gmail.com>
2019-03-27 15:18:40 +0000

    wip on menu

---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: `Salah Application`,
+    description: `An introduction to salah, the second pillar of Islam.`,
+    author: `SalmanK`,
+  },
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `images`,
+        path: `${__dirname}/src/images`,
+      },
+    },
+    `gatsby-transformer-sharp`,
+    `gatsby-plugin-sharp`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // 'gatsby-plugin-offline',
+  ],
+}
---
+++ src/pages/page-2.js
@@ -0,0 +1,15 @@
+import React from "react"
+import { Link } from "gatsby"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+
+const SecondPage = () => (
+  <Layout>
+    <SEO title="Instructions" />
+    <h1>Hi from the second page</h1>
+    <Link to="/">To the homepage</Link>
+  </Layout>
+)
+
+export default SecondPage
