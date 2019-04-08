332ebc57610a7d161070f87c1e83998f65ea0b76
kayote <kay2dan@gmail.com>
2019-04-08 20:24:19 +0100

    working on sass

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,31 @@
 import React from "react";
 import { Link } from "gatsby";
+import { Button, Container } from "bloomer";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 import "../styles/main.sass";
+import "../styles/home.sass";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" keywords={[`salah`, `salaat`, `salat`, `prayer`]} />
-    <div className="btnContainerVertical">
-      <Link className="btnStyle" to="/localSalahtime">
-        Local Salah Time
-      </Link>
-      <Link className="btnStyle" to="/">
-        Guide to Salah
-      </Link>
-    </div>
+    <Container className="has-text-centered">
+      {/* svg title from Nida */}
+    </Container>
+    <Container className="btnContainer has-text-centered">
+      <Button>
+        <Link className="" to="/localSalahtime">
+          Local Salah Time
+        </Link>
+      </Button>
+    </Container>
+    <Container className="btnContainer has-text-centered">
+      <Button>
+        <Link className="" to="/">
+          Guide to Salah
+        </Link>
+      </Button>
+    </Container>
   </Layout>
 );
 
