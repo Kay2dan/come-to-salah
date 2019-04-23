0079c74e1ffd4514db6f695652685ec1521228b4
kayote <kay2dan@gmail.com>
2019-04-23 08:56:07 +0100

    aboutUs page done

--- src/components/TeamProfile.js
+++ src/components/TeamProfile.js
@@ -2,31 +2,20 @@ import React from "react";
 import { Title, Subtitle, Image, Columns, Column } from "bloomer";
 
 const TeamProfile = ({ data }) => {
-  const { name, role, link } = data;
+  const { name, role, link, message } = data;
   const imgClass = link.type !== "none" ? `icon_${link.type}` : "";
-  console.log("imgClass: ", name, role, link, imgClass);
   return (
     <div className="profile">
       <Columns>
         <Column isSize="1/2">
-          <Title>{name}</Title>
-          <Subtitle>{role}</Subtitle>
-          <a href={link.to}>
-            <div
-              isSize="24x24"
-              className={imgClass}
-              style={{ width: "28px", height: "28px" }}
-            />
+          <Title isSize="5">{name}</Title>
+          <Subtitle isSize="6">{role}</Subtitle>
+          <a href={link.to} blank>
+            <div className={imgClass} />
           </a>
         </Column>
         <Column isSize="1/2">
-          <p>
-            Lorem ipsum dolor sit amet, sed graeci accusamus ei. Nec reque
-            utamur delectus no, partem apeirian sea ei. Facilis accumsan
-            consectetuer no his, eum id omnis modus doming. Mea te saepe nostrud
-            gloriatur. Meis velit elaboraret ex ius, cum debet nobis aliquando
-            ex.
-          </p>
+          <p>{message}</p>
         </Column>
       </Columns>
     </div>
--- src/pages/aboutUs.js
+++ src/pages/aboutUs.js
@@ -2,30 +2,33 @@ import React from "react";
 import { Container, Section } from "bloomer";
 import { graphql } from "gatsby";
 import TeamProfile from "../components/TeamProfile";
+import "../styles/aboutUs.sass";
 
 const AboutUs = ({ data }) => {
   const {
     allDataJson: { edges },
   } = data;
   const team = edges.filter(mom => mom.node.title === "Team Profile")[0].node;
-  console.log("team: ", team.profiles);
+  // console.log("team: ", team.profiles);
   return (
     <div className="aboutUsPageWrapper">
-      <Container>
-        <h2>About 'Come To Salah'</h2>
-        <Section>
+      {/* <Container> */}
+      {/* <h2>About 'Come To Salah'</h2> */}
+      <Section>
+        <div className="intro">
           <p>Assalam U Aliekum,</p>
           <p>
             This app has been created to help learn & understand the prayers we
             pray as muslims.
           </p>
-          <div className="profiles">
-            {team.profiles.map((fellow, i) => {
-              return <TeamProfile data={fellow} key={i} />;
-            })}
-          </div>
-        </Section>
-      </Container>
+        </div>
+        <div className="profiles">
+          {team.profiles.map((fellow, i) => (
+            <TeamProfile data={fellow} key={i} />
+          ))}
+        </div>
+      </Section>
+      {/* </Container> */}
     </div>
   );
 };
