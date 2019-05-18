56e6eb76ba0bb1675ada7357424ddbc62acd0495
Salman <salman.khan@coutallure.com>
2019-05-18 18:38:58 +0100

    rewrote the home page tests

--- cypress/integration/home.test.js
+++ cypress/integration/home.test.js
@@ -1,9 +1,18 @@
 describe("Testing the Home page: ", () => {
   it("should visit the page", () => {
     cy.visit("/");
-    cy.get("div.navbar-end div.navbar-item:first").trigger("mouseover");
-    cy.get("div.navbar-dropdown > div.navbar-item:first")
-      .contains("Fajr")
-      .click();
+    // cy.get("div.navbar-end div.navbar-item:first").trigger("mouseover");
+    cy.get("div.navbar-end div.navbar-item")
+      .contains("How To Pray Salah")
+      .trigger("mouseover");
+    cy.get("div.navbar-end div.navbar-item")
+      .contains("How To Pray Salah")
+      .next(".navbar-dropdown")
+      .then($el => {
+        cy.wrap($el).invoke("show");
+        cy.wrap($el)
+          .contains("Fajr")
+          .click();
+      });
   });
 });
